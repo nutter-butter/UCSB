8fa1cf429ff27fcf8d62a07120dedc7cd4fd4cbe
gabryelparedes1 <gaparedes1@my.vcccd.edu>
2025-02-23 15:09:12 -0800

    Add files via upload

--- csvReader.java
+++ csvReader.java
@@ -1,16 +1,21 @@
 
+//import all necessary libraries
+package backend;
+
 import java.io.*;
 import java.util.ArrayList;
 
 public class csvReader {
-    private final String path;
-    private final int col;
+    private String path; //path to the csv file
+    private int col; 
 
+    //constructor for the csvReader object
     public csvReader(String path, int col) {
         this.path = path;
         this.col = col;
     }
 
+    //returns an ArrayList of strings from a certain column
     public ArrayList<String> getColumn() {
         ArrayList<String> list = new ArrayList<>();
         
@@ -24,6 +29,7 @@ public class csvReader {
                     list.add(columns[col]);  
                 }
             }
+        //show the reason if there is an error
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -33,6 +39,7 @@ public class csvReader {
         return list;
     }
 
+    //creates an array list of strings from another string array list
     public ArrayList<String> createList(ArrayList<String> arr) {
         ArrayList<String> list = new ArrayList<>();
         for(int i = 0; i < arr.size(); i++) {
@@ -41,6 +48,7 @@ public class csvReader {
         return list;
     }
 
+    //creates an array list of strings but only gathers one word from the other array list
     public ArrayList<String> createListSpace(ArrayList<String> arr) {
         ArrayList<String> list = new ArrayList<>();
         for(int i = 0; i < arr.size(); i++) {
@@ -49,6 +57,7 @@ public class csvReader {
         return list;
     }
 
+    //creates a new string
     public String createString(String s) {
         String returnString = "";
         for(int i = 0; i < s.length(); i++) {
@@ -61,6 +70,7 @@ public class csvReader {
         return returnString;
     }
 
+    //creates a new string but uses spaces to distinguish
     public String createStringSpace(String s) {
         String returnString = "";
         for(int i = 0; i < s.length(); i++) {
@@ -73,11 +83,13 @@ public class csvReader {
         return returnString;
     }
 
+    //parses through the csv file to make sure that the split doesn't mess things up when there are complex fields
     private String[] parseCsvLine(String line) {
+        //initialization section
         ArrayList<String> columns = new ArrayList<>();
         StringBuilder currentColumn = new StringBuilder();
         boolean insideQuotes = false;
-
+        
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
 
